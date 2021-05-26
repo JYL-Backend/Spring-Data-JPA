fa6d575dc69eeef57c2fbb1366e95e0a6be317d7
JunyoungDev <wnsdud2336@naver.com>
2021-05-26 22:12:03 +0900

    DTO 조회 @Query

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -5,17 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
     @Test
     public void testQuery() throws Exception {
         //given
@@ -26,8 +28,25 @@ class MemberRepositoryTest {
 
         //when
         List<Member> result = memberRepository.findUser("AAA", 10);
-        Assertions.assertThat(result.get(0)).isEqualTo(member1);
 
         //then
+        Assertions.assertThat(result.get(0)).isEqualTo(member1);
+    }
+
+    @Test
+    public void fimdMemberDto() throws Exception {
+        //given
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member member1 = new Member("AAA", 10);
+        member1.setTeam(team);
+        memberRepository.save(member1);
+
+        //when
+        List<MemberDto> memberDtos = memberRepository.findMemberDto();
+
+        //then
+        Assertions.assertThat(memberDtos.get(0).getTeamName()).isEqualTo(team.getName());
     }
 }
